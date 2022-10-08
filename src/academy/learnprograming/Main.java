fbd49142e6b43fe7d259a08bd227de3a590f1523
marsh <m.cowles21191@gmail.com>
2022-10-08 05:18:48 -0400

    second commit

--- src/academy/learnprograming/Main.java
+++ src/academy/learnprograming/Main.java
@@ -4,7 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         boolean isAlien = false;
-        if(isAlien == false)
+        if(isAlien == true) {
             System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if(topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 }

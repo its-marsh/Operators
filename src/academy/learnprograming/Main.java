7d4be7b1d1f503174307cf3060c3f330f789d7d1
marsh <m.cowles21191@gmail.com>
2022-10-08 05:34:43 -0400

    third commit

--- src/academy/learnprograming/Main.java
+++ src/academy/learnprograming/Main.java
@@ -14,10 +14,29 @@ public class Main {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100){
+        int secondTopScore = 80;
+        if ((topScore > secondTopScore) && (topScore < 100)){
             System.out.println("Greater than second top score and less than 100");
         }
 
+        if ((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true.");
+        }
+
+        boolean isCar = false;
+        // if (isCar)
+        // if (!isCar)
+        if (isCar = true){
+            System.out.println("This is not supposed to happen.");
+        }
+
+        boolean wasCar = isCar ? true : false;
+        if (wasCar){
+            System.out.println("wasCar is true");
+        }
+
+        int ageOfClient = 20;
+        boolean isEighteenOrOver = (ageOfClient == 20) ? true : false;
+        System.out.println(isEighteenOrOver);
     }
 }
